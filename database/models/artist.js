b055a54c8d1b4f0cb1c423483af341cc1eea0436
puchk <kpuchalski@gmail.com>
2018-03-12 23:21:26 -0700

    fixed error to export model instead of schema

--- database/models/artist.js
+++ database/models/artist.js
@@ -3,18 +3,18 @@ const AlbumSchema = require('./album');
 const Schema = mongoose.Schema;
 
 const ArtistSchema = new Schema({
-	name: String,
-	age: Number,
-	yearsActive: Number,
-	image: String,
-	genre: String,
-	website: String,
-	netWorth: Number,
-	labelName: String,
-	retired: Boolean,
-	albums: [AlbumSchema]
+  name: String,
+  age: Number,
+  yearsActive: Number,
+  image: String,
+  genre: String,
+  website: String,
+  netWorth: Number,
+  labelName: String,
+  retired: Boolean,
+  albums: [AlbumSchema]
 });
 
 const Artist = mongoose.model('artist', ArtistSchema);
 
-module.exports = ArtistSchema;
+module.exports = Artist;
