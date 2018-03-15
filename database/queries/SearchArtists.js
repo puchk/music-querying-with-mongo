45060b9afe22c54db9eb49cd2a80a5f4768b4f0e
puchk <kpuchalski@gmail.com>
2018-03-15 12:56:32 -0700

    displays artists and able to sort by name, age, or number of albums

--- database/queries/SearchArtists.js
+++ database/queries/SearchArtists.js
@@ -9,4 +9,22 @@ const Artist = require('../models/artist');
  * @return {promise} A promise that resolves with the artists, count, offset, and limit
  */
 module.exports = (criteria, sortProperty, offset = 0, limit = 20) => {
+
+	const query = Artist.find({})
+		// const sortOrder = {};
+		// sortOrder[sortProperty] = 1;
+		// same thing ^
+		.sort({ [sortProperty]: 1 })
+		.skip(offset)
+		.limit(limit);
+
+	return Promise.all([query, Artist.count()])
+		.then((results) => {
+			return {
+				all: results[0],
+				count: results[1],
+				offset: offset,
+				limit: limit
+			};
+		});
 };
